cf1168b79c5e7edacb28b63a7e3318af23f9d66d
Yann Lorthioir <y_lorthioir@msn.com>
2024-03-26 10:27:31 +0100

    Add Lombok

--- src/main/java/be/technobel/demojpastreamer/Javanais.java
+++ src/main/java/be/technobel/demojpastreamer/Javanais.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "javanais")
 public class Javanais {
     @Id
@@ -12,27 +13,4 @@ public class Javanais {
     private String name;
     private String commentaire;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCommentaire() {
-        return commentaire;
-    }
-
-    public void setCommentaire(String commentaire) {
-        this.commentaire = commentaire;
-    }
 }
